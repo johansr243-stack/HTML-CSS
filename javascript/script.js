eb07f53871f4f559b8c4f25044408da581e514a0
Johan Daniel Patiño <johansr243@gmail.com>
2026-03-25 20:10:06 -0500

    edadperro

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+
+let nombre = prompt('cual es el nombre de tu perro')
+let edad = Number(prompt('cuantos años tiene tu perro'))
+
+let edadperro = edad * 7
+
+console.log(('la edad de tu perro es ' + edadperro))

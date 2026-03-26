8d50822eeafc5291a67d75ba5d2e217da0369401
Johan Daniel Patiño <johansr243@gmail.com>
2026-03-25 20:29:30 -0500

    areacuadrado

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 
 let nombre = prompt('cual es tu nombre')
-let cuadradoalto = Number(prompt('tu cuadrado mide de alto'))
-let cuadradoancho = Number(prompt('tu cuadrado mide de ancho'))
+let altura = Number(prompt('tu cuadrado mide de alto'))
+let base = Number(prompt('tu cuadrado mide de ancho'))
 
-let area = cuadradoalto * cuadradoancho
+let area = altura * base
 
 console.log('el area del cuadrado es: ' + area)
 
